a6f2a14c6eb740aa984edc083ea42b4e5b088c2f
Mike Ng <mike@kj6aku.net>
2023-02-04 22:34:53 -0800

    starting point, known good Bruno Levy example

---
+++ icestick/makeit.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+# from Bruno Levy's basic serial example
+# https://github.com/BrunoLevy/learn-fpga/tree/master/Basic/ICESTICK/Serial
+
+PROJECTNAME=serial
+VERILOGS="$PROJECTNAME.v uart.v"
+yosys -q -p "synth_ice40 -top $PROJECTNAME -json $PROJECTNAME.json" $VERILOGS || exit
+nextpnr-ice40 --json $PROJECTNAME.json --pcf $PROJECTNAME.pcf --asc $PROJECTNAME.asc --freq 12 --hx1k --package tq144 || exit
+icepack $PROJECTNAME.asc $PROJECTNAME.bin || exit
+iceprog $PROJECTNAME.bin || exit
+echo DONE.
+
+
+
+
